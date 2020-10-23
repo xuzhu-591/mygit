7088a2023f37dc440f9d1b397d6a3e2d892334d1
zhuxu <ZHUXU591@163.com>
2020-10-23 14:37:11 +0800

    a simple cpp exmaple

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+//#include<time.h>
+//#include<ctime>
+//
+//#include<math.h>
+//#include<cmath>
+
+int main(void)
+{
+	/*
+	cout:标准输出流对象
+	<<:拼接输出内容
+	endl:end line 刷新缓存区 并换行
+	*/
+	cout << "hello world" << 234 << endl;
+	return EXIT_SUCCESS;
+}
